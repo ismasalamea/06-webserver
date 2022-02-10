2685f28ad9e6385ff0d392d7d33f163828ec633d
ismasalamea <ismasalamea>
2022-02-10 14:25:37 -0500

    video94

--- app.js
+++ app.js
@@ -1,28 +1,48 @@
-const express = require('express')
-const app = express();
-const port = 8080;
-app.set('view engine', 'hbs');
 
+const hbs = require('hbs');
+const express = require('express')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+
+//Handlebars
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 //Servir contenido estatico
 //Hacer publica una carpeta
 app.use(express.static('public'));
 
 app.get('/',  (req, res) => {
-  res.render('home');
+  res.render('home', {
+    nombre: 'Fernando Herrera',
+    titulo: 'Curso de Node'
+  });
+
 })
 
 
 app.get('/generic',  (req, res) => {
-    res.sendFile( __dirname + '/public/generic.html' )
+    res.render('generic', {
+      nombre: 'Fernando Herrera',
+      titulo: 'Curso de Node'
+    });
+    //res.sendFile( __dirname + '/public/generic.html' )
 })
  
 app.get('/elements',  (req, res) => {
-  res.sendFile( __dirname + '/public/elements.html' )
+  res.render('elements', {
+    nombre: 'Fernando Herrera',
+    titulo: 'Curso de Node'
+  });
+ // res.sendFile( __dirname + '/public/elements.html' )
 })
 
 
 app.get('*',  (req, res) => {
-   res.sendFile( __dirname + '/public/404.html' )
+   
+  res.sendFile( __dirname + '/public/404.html' )
 })
 
-app.listen(8080)
+app.listen(port)
