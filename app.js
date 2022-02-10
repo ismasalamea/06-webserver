f98d90860f8e80dadf9a1256891a530bd9f18ec8
ismasalamea <ismasalamea>
2022-02-09 22:28:44 -0500

    video91

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express')
 const app = express();
 const port = 8080;
+app.set('view engine', 'hbs');
 
 //Servir contenido estatico
+//Hacer publica una carpeta
 app.use(express.static('public'));
 
 app.get('/',  (req, res) => {
-  res.send('Home page')
+  res.render('home');
 })
 
-app.get('/hola-mundo',  (req, res) => {
-    res.send('Hola mundo en su respectiva ruta')
-  })
+
+app.get('/generic',  (req, res) => {
+    res.sendFile( __dirname + '/public/generic.html' )
+})
+ 
+app.get('/elements',  (req, res) => {
+  res.sendFile( __dirname + '/public/elements.html' )
+})
+
 
 app.get('*',  (req, res) => {
-   res.send('404 page not found')
+   res.sendFile( __dirname + '/public/404.html' )
 })
 
 app.listen(8080)
